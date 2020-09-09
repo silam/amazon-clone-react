4ab6b812569a77eeb4192a62e15be0abd1fed7ea
silam <silam@hotmail.com>
2020-09-08 23:09:50 -0500

    React Amazon Clone

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Home.css";
+
+{/* <img 
+alt="All-new Blink Outdoor. Alexa, 
+show me the front door camera. $99.99" 
+src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg" width="1500px" height="600px"></img> */}
+
+function  s() {
+    return (
+        <div className='home'>
+            <div className='home__container'>
+                <img className="home__image" 
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg" alt="" />
+            </div>
+
+            <div className="home__row">
+
+            </div>   
+            <div className="home__row">
+                
+            </div>   
+
+            <div className="home__row">
+                
+            </div>   
+                     
+        </div>
+    )
+}
+
+export default  s
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRNKuzlPwZYpwfPr0ojCwP6-_435BNggs",
+    authDomain: "clone-46a06.firebaseapp.com",
+    databaseURL: "https://clone-46a06.firebaseio.com",
+    projectId: "clone-46a06",
+    storageBucket: "clone-46a06.appspot.com",
+    messagingSenderId: "441509692301",
+    appId: "1:441509692301:web:04ad2f3c3bfd28a22473c4",
+    measurementId: "G-4Z6W8CXZ24"
+  };
