eb54de3c84342c89eb86b89e7fe8198030b029e2
silam <silam@hotmail.com>
2020-09-09 11:54:19 -0500

    react router dom

--- src/Home.js
+++ src/Home.js
@@ -14,21 +14,44 @@ function  s() {
             <div className='home__container'>
                 <img className="home__image" 
                     src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg" alt="" />
+            
+
+                <div className="home__row">
+                    <Product id="111"
+                        title={'The lean startup'} price={19.99}
+                    image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                         rating={5}></Product>
+                    <Product id="112"
+                        title={'The lean startup'} price={19.99}
+                    image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                         rating={5}></Product>
+
+                </div>   
+                <div className="home__row">
+                
+                <Product id="113"
+                        title={'The lean startup'} price={19.99}
+                    image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                         rating={5}></Product>
+                    <Product id="114"
+                        title={'The lean startup'} price={19.99}
+                    image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                         rating={5}></Product>
+                
+                    <Product id="115"
+                            title={'The lean startup'} price={19.99}
+                        image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                            rating={5}></Product>
+                </div>   
+
+                <div className="home__row">
+                <Product id="116"
+                        title={'The lean startup'} price={19.99}
+                    image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg" 
+                         rating={5}></Product>
+
+                </div>   
             </div>
-
-            <div className="home__row">
-                <Product ></Product>
-                <Product ></Product>
-
-            </div>   
-            <div className="home__row">
-                
-            </div>   
-
-            <div className="home__row">
-                
-            </div>   
-
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import './Product.css';
 
-function Product() {
+function Product({id, title, image, price, rating}) {
     return (
-        <div className='product'>
-            <div className='product__info'>
-                    <p>The lean startup</p>
-                    <p className='product__price'>
+        <div className="product">
+            <div className="product__info">
+                    <p>{title}</p>
+                    <p className="product__price">
                         <small>$</small>
-                        <strong>19.99</strong>
-                        </p>
-                    <div className='product__rating'>
-                        <p>*</p> 
-                       <p>*</p>
-                        <p>*</p>
+                        <strong>{price}</strong>
+                    </p>
+                    <div className="product__rating">
 
+                        {Array(rating).fill().map((_, i) => 
+                        (
+                            <p>*</p> 
+                        ))}
+                        
+                       
                     </div>
+
+                    
             </div>
+
+            <img alt="Go wild with trendy prints" 
+                    src={image} />                
+                    
+                    <button>Add to Basket</button>
         </div>
     )
 }
+
+
 
 export default Product
